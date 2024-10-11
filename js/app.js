d13d41705bef53b171d942b580228ab9777b8e45
Sorvani <sorvani@gmail.com>
2024-10-11 12:25:51 -0500

    multi-line likely working

--- js/app.js
+++ js/app.js
@@ -75,14 +75,14 @@ function submitCorrection() {
     
     // Construct the issue title and body
     const issueTitle = `Update${explorerName ? ` for ${encodeURIComponent(explorerName)}` : ''}`;
-    const issueBody = `Correction Details:\n\n${encodeURIComponent(correctionDetails).replace(/%0A/g, '\n')}`;
-    
+    const issueBody = `Update Details:\n${correctionDetails}`;
+ 
     const githubUsername = 'sorvani'; 
     const githubRepo = 'dgenesisinfo';
     const labels = encodeURIComponent(correctionType);  // Use the correction type as a label
     
     // Construct the GitHub issue URL
-    const githubIssueUrl = `https://github.com/${githubUsername}/${githubRepo}/issues/new?title=${issueTitle}&body=${issueBody}&labels=${labels}`;
+    const githubIssueUrl = `https://github.com/${githubUsername}/${githubRepo}/issues/new?title=${issueTitle}&body=${encodeURIComponent(issueBody)}&labels=${labels}`;
     
     // Redirect user to GitHub to finalize the issue
     window.open(githubIssueUrl, '_blank');
