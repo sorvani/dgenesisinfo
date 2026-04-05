d5e0bcca77e4eab20ae52faaad670c255030304d
Jared Busch <sorvani@gmail.com>
2026-04-05 10:11:20 -0500

    Reword description of character page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -5,7 +5,7 @@ import { CharactersGrid } from "@/components/CharactersGrid";
 export const metadata: Metadata = {
   title: "Cast of Characters",
   description:
-    "Database of all known characters and explorers from D-Genesis.",
+    "Database of characters and explorers from D-Genesis.",
 };
 
 export default function CharactersPage() {
@@ -17,7 +17,7 @@ export default function CharactersPage() {
       <div className="page-header">
         <h1>Cast of Characters</h1>
         <p>
-          A complete roster containing {characters.length} known
+          Aiming to be a complete roster, and currently containing {characters.length} known
           characters and explorers. Click a character to view their biography and metadata.
         </p>
       </div>
